bef93ba5fe79cfe5feeb5be6d28c0bd730037a23
Prabod Ashan <prabodashan14@gmail.com>
2022-06-22 00:02:17 +0530

    first commit

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Home() {
+  return (
+    <div className="bg-image middle-btn">
+      <h1>Internet Of Things</h1>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing elements index tempor
+        ipsum dolor sit amet sit amet, consectetur adipiscing elements index
+        tempor ipsum dolor sit amet, consectetur
+      </p>
+      <Button className="btn" variant="warning" size="lg">
+        Get Started
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
